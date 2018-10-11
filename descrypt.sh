20caa41b22d41c6f621657cd9aef2005e498c620
donegaan <donegaan@tcd.ie>
2018-10-11 20:27:58 +0100

    added echos to know what its doing

--- descrypt.sh
+++ descrypt.sh
@@ -5,10 +5,13 @@ echo "Running DES hashcat"
 #DES cracking
 
 # dictionary attack
+echo "Rockyou"
 hashcat -m 1500 -a 3 -O ./hashes/des.txt ./wordlists/rockyou.txt
 
 # mask attack for 5 lowercase letters
+echo "Mask"
 hashcat -m 1500 -a 3 -O ./hashes/des.txt ?l?l?l?l?l
 
 # concatentation attack
+echo "Concat"
 hashcat -m 1500 -a 1 -O ./hashes/des.txt ./wordlists/four.txt ./wordlists/four.txt
