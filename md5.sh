25dc373c4834c4d488753158b98dcbe0a5a72a94
donegaan <donegaan@tcd.ie>
2018-10-12 09:49:53 +0100

    Made path to my potfile

--- md5.sh
+++ md5.sh
@@ -6,12 +6,12 @@ echo "Running MD5crypt hashcat"
 
 # dictionary attack
 echo "Rockyou"
-hashcat -m 500 -a 0 -O -w 4 ./hashes/md5.txt ./wordlists/rockyou.txt
+hashcat -m 500 -a 0 -O -w 4 --potfile-path=mypot.potfile ./hashes/md5.txt ./wordlists/rockyou.txt
 
 # mask attack for 5 lowercase letters
 echo "Mask"
-hashcat -m 500 -a 3 -O -w 4 ./hashes/md5.txt ?l?l?l?l?l
+hashcat -m 500 -a 3 -O -w 4 --potfile-path=mypot.potfile ./hashes/md5.txt ?l?l?l?l?l
 
 # concatentation attack
 echo "Concat"
-hashcat -m 500 -a 1 -O -w 4 ./hashes/md5.txt ./wordlists/four.txt ./wordlists/four.txt
+hashcat -m 500 -a 1 -O -w 4 --potfile-path=mypot.potfile ./hashes/md5.txt ./wordlists/four.txt ./wordlists/four.txt
--- pbk.sh
+++ pbk.sh
@@ -6,12 +6,12 @@ echo "Running pbk hashcat"
 
 # dictionary attack
 echo "Rockyou"
-hashcat -m 500 -a 0 -O -w 4 ./hashes/pbk.txt ./wordlists/rockyou.txt
+hashcat -m 500 -a 0 -O -w 4 --potfile-path=mypot.potfile ./hashes/pbk.txt ./wordlists/rockyou.txt
 
 # mask attack for 5 lowercase letters
 echo "Mask"
-hashcat -m 500 -a 3 -O -w 4 ./hashes/pbk.txt ?l?l?l?l?l
+hashcat -m 500 -a 3 -O -w 4 --potfile-path=mypot.potfile ./hashes/pbk.txt ?l?l?l?l?l
 
 # concatentation attack
 echo "Concat"
-hashcat -m 500 -a 1 -O -w 4 ./hashes/pbk.txt ./wordlists/four.txt ./wordlists/four.txt
+hashcat -m 500 -a 1 -O -w 4 --potfile-path=mypot.potfile ./hashes/pbk.txt ./wordlists/four.txt ./wordlists/four.txt
