3f50e29a7129b813571058ff0f15c72c06890dda
Andrew Donegan <donegaan@tcd.ie>
2018-10-14 00:48:56 +0100

    Update pbk.sh

--- pbk.sh
+++ pbk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./john --wordlist=concatWords.txt --format=PBKDF2-HMAC-SHA256-opencl pbk.txt --pot=mypot.potfile
+./john --wordlist=pbkWordlist.txt --format=PBKDF2-HMAC-SHA256-opencl pbk.txt --pot=mypot.potfile --stdout=5
 
 ehco "getting rockyou"
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
