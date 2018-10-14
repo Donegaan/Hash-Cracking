c93e48afc6c828e9a75be34f1466d82573676639
donegaan <donegaan@tcd.ie>
2018-10-14 12:49:57 +0100

    Added attack mode to DES concat attack

--- descrypt.sh
+++ descrypt.sh
@@ -13,4 +13,4 @@ hashcat -m 1500 -a 3 -O -w 4 --potfile-path=mypot.potfile ./hashes/des.txt ?l?l?
 
 # concatentation attack
 echo "Concat"
-hashcat -m 1500 -O -w 4 --potfile-path=mypot.potfile ./hashes/des.txt ./wordlists/concatWords.txt
+hashcat -m 1500 -a 0 -O -w 4 --potfile-path=mypot.potfile ./hashes/des.txt ./wordlists/concatWords.txt
